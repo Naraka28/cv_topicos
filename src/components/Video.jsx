d67c26a57b7a528e612442e7a14ae38308acc445
Naraka28 <eildan28@hotmail.com>
2024-10-16 21:46:30 -0700

    fix video component

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,21 +2,22 @@
 import { Title } from "./Title";
 export function Video() {
   return (
-    <div className="flex flex-col md:flex-row justify-center mb-16 ">
-      <div className="">
-        <Title>Videocurrículum</Title>
-        <iframe
-          className="justify-center"
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/7tllUDX0jMo?si=GMZZaAnCORxhckDJ"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
-        ></iframe>
-      </div>
-    </div>
+   
+      <div className="flex flex-col md:flex-row justify-center my-20 ">
+        <div className="w-full md:w-7/12">
+            <Title>Videocurrículum</Title>
+            <iframe
+            className="w-full h-96 lg:w-96 lg:h-96 lg:mx-auto"
+            src="https://www.youtube.com/embed/7tllUDX0jMo?si=GMZZaAnCORxhckDJ"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+            >
+            </iframe>
+        </div>
+
+        </div>
   );
 }
